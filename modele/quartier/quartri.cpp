a5b2ad77d50814b82e0abdc0dd9d3dc339cbe8c1
gitgamagorajerome <jerome.baret@univ-lyon2.fr>
2015-11-23 09:13:15 +0100

    fix height

--- modele/quartier/quartri.cpp
+++ modele/quartier/quartri.cpp
@@ -326,7 +326,7 @@ std::pair<Vector2D, Vector2D> QuarTri::traiteCote(const Vector2D& pp2, const Vec
             /  \
        pp2 /____\ pp1
     */
-    float height3 = dot(pp3-pp1,pp2-pp1)/distance(pp2,pp1);//((pp3 - pp1) / (pp2 - pp1)).getNorm() / (pp2 - pp1).getNorm();
+    float height3 = dot(pp3-pp1,pp2-pp1)/distance(pp2,pp1);//((pp3 - pp1) ^ (pp2 - pp1)).getNorm() / (pp2 - pp1).getNorm();
     if (height3 <= 2 * MIN_DIM_BAT)
         return std::make_pair(pp1, pp2); // ou exclure la petite bande ?
     auto distribution = make_Unidistrib(MIN_DIM_BAT, height3 - MIN_DIM_BAT);
