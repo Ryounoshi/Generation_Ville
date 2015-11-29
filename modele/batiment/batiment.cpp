b35d1b10a70599350d9e5cef8c4738801e0fdf4d
RudiWelter <rudi.welter@laposte.net>
2015-11-29 19:23:10 +0100

    modif restriction

--- modele/batiment/batiment.cpp
+++ modele/batiment/batiment.cpp
@@ -18,8 +18,8 @@ Mesh Batiment::generate()
     if(orientation(Vector2D(XY(_p0)),Vector2D(XY(_p1)),Vector2D(XY(_p2))) > 0)
         return Mesh();
     //if(true){
-    //if(longueur <= largeur*2 && largeur <= longueur*2 && (rand()%100 < _hexaPourcent)){
-    if((rand()%100 < _hexaPourcent)){
+    if(longueur <= largeur*2 && largeur <= longueur*2 && (rand()%100 < _hexaPourcent)){
+    //if((rand()%100 < _hexaPourcent)){
         Vector3D dirlongueur = (_p1-_p0).normalized();
         Vector3D dirlargeur = (_p3-_p0).normalized();
         Vector3D gravity = (_p0 + _p1 + _p2 + _p3)/4;
